b56216a0e4dbed8c11e1f0e56ba6f4f028798385
KhizarAlam20 <khizar.alam20@gmail.com>
2024-01-13 22:24:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.coroutines
+
+import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.contextaware.withContextAvailable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var img: ImageView
+    lateinit var btn: Button
+    lateinit var count: Button
+    lateinit var tvCount: TextView
+    var counter: Int = 0
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        img = findViewById(R.id.img)
+        btn = findViewById(R.id.btn)
+        count = findViewById(R.id.count)
+        tvCount = findViewById(R.id.tvCount)
+
+        btn.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+
+                try {
+                    var URLs = URL("https://imgur.com/xzVxzEo.jpg")
+                    var bitmap = BitmapFactory.decodeStream(URLs.openStream())
+
+                    withContext(Dispatchers.Main) {
+                        img.setImageBitmap(bitmap)
+                    }
+
+                } catch (e: Exception) {
+                    Log.d("myTag", "Coroutine : ${e}")
+//                    Toast.makeText(this,"corotine : [${e}]").show()
+                }
+            }
+        }
+
+
+
+        count.setOnClickListener {
+            counter += 1
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    withContext(Dispatchers.Main) {
+                        tvCount.text = counter.toString()
+                    }
+                } catch (e: Exception) {
+                    Log.d("myTag", "Coroutine Counter : ${e}")
+//                    Toast.makeText(this,"corotine : [${e}]").show()
+                }
+            }
+        }
+
+    }
+}
